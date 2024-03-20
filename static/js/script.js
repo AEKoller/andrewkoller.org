6011f93288842cea3ab6713b734ff370bed13b37
AEKoller <andrew@andrewkoller.org>
2024-03-20 16:43:07 -0500

    v4.3

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,42 @@
 // slideshow script
 document.addEventListener('DOMContentLoaded', function() {
-    const images = document.querySelectorAll('.slideshow-image');
-    let currentImageIndex = 0;
-  
-    function showNextImage() {
-      images[currentImageIndex].style.opacity = 0;
-      currentImageIndex = (currentImageIndex + 1) % images.length;
-      images[currentImageIndex].style.opacity = 1;
-    }
-  
-    images.forEach((image, index) => {
-      if (index !== 0) {
-        image.style.opacity = 0;
-      }
-    });
-  
+  const images = document.querySelectorAll('.slideshow-image');
+  let currentImageIndex = 0;
+  let imagesLoaded = 0;
+
+  function showNextImage() {
+    images[currentImageIndex].style.opacity = 0;
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    images[currentImageIndex].style.opacity = 1;
+  }
+
+  function startSlideshow() {
     setInterval(showNextImage, 5000); // Change image every 5 seconds
+  }
+
+  images.forEach((image, index) => {
+    if (index !== 0) {
+      image.style.opacity = 0;
+    }
+    
+    // Preload the image
+    const img = new Image();
+    img.src = image.src;
+    img.onload = function() {
+      imagesLoaded++;
+      if (imagesLoaded === images.length) {
+        // All images are loaded, start the slideshow
+        setTimeout(startSlideshow, 2000); // Delay the start of the slideshow by 2 seconds
+      }
+    };
+  });
+});
+
+  // Overlay fade-in script
+  window.addEventListener('load', function() {
+    var overlayContainer = document.querySelector('.overlay-container');
+    
+    setTimeout(function() {
+      overlayContainer.classList.add('visible');
+    }, 1300);
   });
